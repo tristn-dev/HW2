b431d134b20f280ab3566a099a0d2f3d83cd0cdd
tristn-dev <115331845+tristn-dev@users.noreply.github.com>
2023-10-05 15:51:13 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+password = input()
+password = password.replace('i','!')
+password = password.replace('a','@')
+password = password.replace('m','M')
+password = password.replace('B','8')
+password = password.replace('o','.')
+password = password + "q*s"
+print(password)
