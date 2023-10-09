ba167c957ba5b58b690c01482fb818903aed192d
tristn-dev <115331845+tristn-dev@users.noreply.github.com>
2023-10-08 22:50:46 -0500

    Add files via upload

---
+++ HW2_6.22.py
@@ -0,0 +1,27 @@
+# Kelvin Sia 2122601
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = int(input())
+e = int(input())
+f = int(input())
+
+solution_found = False
+
+for x in range(-10, 11):
+    for y in range(-10, 11):
+        if (a * x) + (b * y) != c:
+            continue
+        if (d * x) + (e * y) == f:
+            print(x, y)
+            solution_found = True
+            break  # Break out of the inner loop if a solution is found
+if not solution_found:
+    print("No solution")
+
+      
+    
+      
---
+++ HW2_8.10.py
@@ -0,0 +1,11 @@
+# Kelvin Sia 2122601
+
+
+word = str(input())
+nospace_word = word.replace(" ", "")
+new = nospace_word[::-1]
+
+if nospace_word == new:
+  print(word, "is a palindrome")
+else:
+  print(word, "is not a palindrome")
